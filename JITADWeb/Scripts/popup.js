39f4e84086eb5555aee50f75d2295457fc04de2e
manio143 <marian.dziubiak@gmail.com>
2015-03-26 08:52:54 +0100

    55631r5

--- JITADWeb/Scripts/popup.js
+++ JITADWeb/Scripts/popup.js
@@ -28,7 +28,7 @@ jQuery.fn.extend({
 		box.find('a.usr_popup_close').click(function(e)
 			{	e.preventDefault();
 					createCookie(o.cookie_name,1,1);
-					window.location.href = "http://jitad2015.waw.pl/Konto/";
+					window.location = "http://jitad2015.waw.pl/Konto/";
 					box.fadeOut(o.time);
 		});
         box.find('a.usr_popup_link').click(function(e)
